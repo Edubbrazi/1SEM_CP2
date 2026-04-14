e98efb0b242dde273cc3ca55b7f1aa149dcd9ed6
Edubbrazi <edubbrazi@mail.com>
2026-04-14 14:33:36 -0300

    finalizado o codigo

--- Exercicios/pcp_1sem_cp2_ex01.py
+++ Exercicios/pcp_1sem_cp2_ex01.py
@@ -1,13 +1,12 @@
 #Coleta de dados
-from numba.core.cgutils import printf
-
 cod_origem = int(input("Insira o código de origem da carga do caminhão(1-5): "))
 cod_carga = int(input("Insira o código da carga(10-40): "))
 peso_carga = float(input("Insira o peso da carga do caminhão em toneladas: "))
 
-#conversão de toneladas para quilos
+#conversão de toneladas para quilos e preços
 
 peso = peso_carga * 1000
+print(f"O valor da carga em quilos: {peso}")
 if 10<= cod_carga <= 20:
     preco = peso * 100
     print(f"O valor da carga respectivamente a seu código: {preco}")
@@ -18,3 +17,12 @@ elif 31 <= cod_carga <= 40:
     preco = peso * 340
     print(f"O valor da carga respectivamente a seu código: {preco}")
 #Valor do imposto
+taxas = [0.35, 0.25, 0.15, 0.05, 0.0]
+
+valor_imposto = preco * taxas[cod_origem - 1]
+print(f"O valor do imposto da carga: {valor_imposto}")
+
+#Valor total
+
+valor_total = preco + valor_imposto
+print(f"O valor total da carga: {valor_total}")
